68cdb3692870e489ec5f13c251ad388fae55cc6f
Ram Goel <rgoel766@gmail.com>
2023-09-20 18:24:16 +0530

    removed eslint warning

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,8 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <title>Ram Goel</title>
+      <Head title='Ram Goel'>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
       </Head>
       <body className='bg-slate-900 leading-relaxed text-slate-400 antialiased selection:bg-teal-300 selection:text-teal-900'>
