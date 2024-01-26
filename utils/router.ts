660bac477711d36eb2928dd1d4bdc13e84122854
Ram Goel <rgoel766@gmail.com>
2024-01-26 23:34:01 +0530

    feat: useRefBlog

--- utils/router.ts
+++ utils/router.ts
@@ -1,5 +1,7 @@
 import momentBlogData from '@/data/blogs/moment.json'
+import useRefBlogData from '@/data/blogs/useRef.json'
 
 export const BlogRouter:any = {
     "using-moment-in-react": momentBlogData,    
+    "avoid-rerender-with-useRef":useRefBlogData
 }
