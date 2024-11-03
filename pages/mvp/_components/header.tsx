201951b0d94a86aab9ae066426c31c010b4606b1
Ram Goel <rgoel766@gmail.com>
2024-11-03 10:50:21 +0530

    feat: name site

--- pages/mvp/_components/header.tsx
+++ pages/mvp/_components/header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
         <div className="flex text-white flex-row items-center justify-center fixed top-0 left-0 w-full h-24 bg-neutral-900 z-50">
             <div className="flex flex-row items-center justify-between w-11/12 md:w-1/2">
                 <Link href={'#hero'}>
-                    <h1 className="text-2xl font-bold">Ram Goel.</h1>
+                    <h1 className="text-2xl font-bold">rammlabs</h1>
                 </Link>
 
                 {/* <div className="flex flex-row gap-6">
