43b6ee4f538ec05805ea64ec82e8b15c67c5dec8
Ram Goel <rgoel766@gmail.com>
2025-01-21 01:13:21 +0530

    fix: meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,39 +1,57 @@
 import { Head, Html, Main, NextScript } from 'next/document'
 
-export const metadata = {
-    title: 'Ram Goel - GenAI and Full Stack Developer',
-    description:
-        'I work on GenAI and full-stack development. Building Noterr to help organize the internet.',
-    image: 'https://ramgoel.com/api/og?title=Ram Goel',
-    url: 'https://ramgoel.com',
-    siteName: 'Ram Goel',
-    type: 'website',
-    creator: '@theramgoel',
-    card: 'summary_large_image',
-    themeColor: '#18181B',
-    author: 'Ram Goel',
-    keywords: 'Ram Goel, Ram, Goel, ramgoel, ramgoel.com, ramgoel.dev',
-    robots: 'index, follow',
-    openGraph: {
-        title: 'Ram Goel - GenAI and Full Stack Developer',
-        description:
-            'I work on GenAI and full-stack development. Building Noterr to help organize the internet.',
-        images: [{ url: 'https://ramgoel.com/api/og?title=Ram Goel' }],
-    },
-    twitter: {
-        card: 'summary_large_image',
-        creator: '@theramgoel',
-        title: 'Ram Goel - GenAI and Full Stack Developer',
-        description:
-            'I work on GenAI and full-stack development. Building Noterr to help organize the internet.',
-        images: [{ url: 'https://ramgoel.com/api/og?title=Ram Goel' }],
-    },
-}
-
 export default function Document() {
     return (
         <Html lang="en">
-            <Head></Head>
+            <Head>
+                <meta charSet="UTF-8" />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1.0"
+                />
+                <meta
+                    name="title"
+                    content="Ram Goel - GenAI and Full Stack Developer"
+                    data-rh="true"
+                />
+                <meta
+                    name="description"
+                    content="I work on GenAI and full-stack development. Building Noterr to help organize the internet."
+                    data-rh="true"
+                />
+                <meta name="author" content="Ram Goel" />
+                <link rel="author" href="https://ramgoel.com" data-rh="true" />
+                <meta
+                    property="og:title"
+                    content="Ram Goel - GenAI and Full Stack Developer"
+                />
+                <meta
+                    property="og:description"
+                    content="I work on GenAI and full-stack development. Building Noterr to help organize the internet."
+                />
+                <meta
+                    property="og:image"
+                    content="https://ramgoel.com/api/og?title=Ram Goel"
+                />
+                <meta property="og:url" content="https://ramgoel.com" />
+                <meta
+                    name="twitter:title"
+                    content="Ram Goel - GenAI and Full Stack Developer"
+                />
+                <meta
+                    name="twitter:description"
+                    content="I work on GenAI and full-stack development. Building Noterr to help organize the internet."
+                />
+                <meta
+                    name="twitter:image"
+                    content="https://ramgoel.com/api/og?title=Ram Goel"
+                />
+                <meta name="twitter:site" content="@theramgoel" />
+                <meta name="twitter:creator" content="@theramgoel" />
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="theme-color" content="#18181B" />
+                <meta property="og:site_name" content="Ram Goel" />
+            </Head>
             <body className="">
                 <Main />
                 <NextScript />
