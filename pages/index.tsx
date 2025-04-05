e0f2125930649ce9b81ad753387d459dfb727e05
Ram Goel <rgoel766@gmail.com>
2025-04-05 18:48:32 +0530

    feat: make more minimalisitic

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,16 @@
 import CustomLink from '@/components/CustomLink'
-import { projects, SKILLS } from '@/utils/data'
-import { Globe2 } from 'lucide-react'
+import { projects } from '@/utils/data'
+import { motion } from 'framer-motion'
+import { ArrowDownRight, Globe2 } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
+import { useState } from 'react'
 import { FaGoogleDrive } from 'react-icons/fa'
 import { SiGithub } from 'react-icons/si'
 
 export default function Home() {
+    const [isOpen, setIsOpen] = useState(false)
+
     return (
         <div className="flex flex-col gap-2 w-full min-[1800px]:w-[65%]">
             <div className="flex items-center gap-3">
@@ -40,8 +44,8 @@ export default function Home() {
 
             <div className="flex flex-col text-justify text-sm leading-loose tracking-wide gap-5">
                 <p className="text-neutral-400">
-                    My interests lies around GenAI, web development and solving
-                    actual problems using code. I&apos;m building{' '}
+                    A Frontend Engineer, deeply passionate about GenAI. I&apos;m
+                    building{' '}
                     <CustomLink href="https://noterr.ramgoel.com">
                         Noterr
                     </CustomLink>{' '}
@@ -49,12 +53,13 @@ export default function Home() {
                 </p>
 
                 <p className="text-neutral-400">
-                    I wrote my first line of code in 2019. I&apos;ve spent my
-                    college doing internships, freelance projects, and
-                    attending/organizing developer events. In 2024, I graduated
-                    from a computer science degree from India, and joined as a
-                    software engineer at{' '}
-                    <CustomLink href="https://getconch.ai/">ConchAI</CustomLink>
+                    Wrote my first line of code in 2019, spent my college doing
+                    internships, freelance projects, and attending/organizing
+                    developer events.{' '}
+                    <span className="text-neutral-300">
+                        Currently making &quot;building AI agents easier for
+                        everyone&quot;
+                    </span>
                 </p>
 
                 <p className="text-neutral-400">
@@ -77,7 +82,7 @@ export default function Home() {
                 </p>
             </div>
 
-            <div className="flex items-center mt-5 gap-3 flex-wrap">
+            {/* <div className="flex items-center mt-5 gap-3 flex-wrap">
                 {SKILLS.map((item) => {
                     return (
                         <div
@@ -89,59 +94,82 @@ export default function Home() {
                         </div>
                     )
                 })}
-            </div>
+            </div> */}
 
-            <h1 className="mt-5 text-lg">Work</h1>
-            <div className="text-md leading-loose flex flex-col gap-4">
-                <div className="flex flex-col lg:w-3/4 gap-6">
-                    {projects.map((item) => {
-                        return (
-                            <div key={item.id} className="flex gap-3 flex-col">
-                                <h3 className="text-sm">
-                                    {item.title}{' '}
-                                    {item.users
-                                        ? `(${item.users}+ users)`
-                                        : item.stars
-                                          ? `(${item.stars} stars)`
-                                          : ''}
-                                </h3>
-                                <p className="text-sm mt-[-7px] text-neutral-400">
-                                    {item.content}
-                                </p>
+            <h1
+                className={`border-b mt-5 mb-3 inline-flex text-sm items-center gap-1 border-neutral-800 border-dashed text-neutral-300 hover:text-yellow-200 transition-all cursor-pointer w-fit`}
+                onClick={() => setIsOpen(!isOpen)}
+            >
+                See what I built <ArrowDownRight size={15} />{' '}
+            </h1>
+            <motion.div
+                initial={{ height: 0, opacity: 0 }}
+                animate={{
+                    height: isOpen ? 'auto' : 0,
+                    opacity: isOpen ? 1 : 0,
+                }}
+                transition={{ duration: 0.3 }}
+                className="overflow-hidden"
+            >
+                <div className="text-md leading-loose flex flex-col gap-4">
+                    <div className="flex flex-col lg:w-3/4 gap-6">
+                        {projects.map((item) => {
+                            return (
+                                <div
+                                    key={item.id}
+                                    className="flex gap-3 flex-col"
+                                >
+                                    <h3 className="text-sm">
+                                        {item.title}{' '}
+                                        {item.users
+                                            ? `(${item.users}+ users)`
+                                            : item.stars
+                                              ? `(${item.stars} stars)`
+                                              : ''}
+                                    </h3>
+                                    <p className="text-sm mt-[-7px] text-neutral-400">
+                                        {item.content}
+                                    </p>
 
-                                <div className="flex gap-4 items-center">
-                                    {item.type === 'oss' ? (
-                                        <CustomLink href={item.url}>
-                                            <SiGithub size={15} />{' '}
-                                            <p className="text-sm">
-                                                #{item.url.split('/').at(-1)}
-                                            </p>
-                                        </CustomLink>
-                                    ) : null}
-                                    {item.github ? (
-                                        <CustomLink href={item.github}>
-                                            <SiGithub size={15} />{' '}
-                                            <p className="text-sm">View Repo</p>
-                                        </CustomLink>
-                                    ) : null}
-                                    {item.url && item.type !== 'oss' ? (
-                                        <CustomLink
-                                            href={
-                                                Array.isArray(item.url)
-                                                    ? item.url[0]
-                                                    : item.url
-                                            }
-                                        >
-                                            <Globe2 size={15} />{' '}
-                                            <p className="text-sm">Website</p>
-                                        </CustomLink>
-                                    ) : null}
+                                    <div className="flex gap-4 items-center">
+                                        {item.type === 'oss' ? (
+                                            <CustomLink href={item.url}>
+                                                <SiGithub size={15} />{' '}
+                                                <p className="text-sm">
+                                                    #
+                                                    {item.url.split('/').at(-1)}
+                                                </p>
+                                            </CustomLink>
+                                        ) : null}
+                                        {item.github ? (
+                                            <CustomLink href={item.github}>
+                                                <SiGithub size={15} />{' '}
+                                                <p className="text-sm">
+                                                    View Repo
+                                                </p>
+                                            </CustomLink>
+                                        ) : null}
+                                        {item.url && item.type !== 'oss' ? (
+                                            <CustomLink
+                                                href={
+                                                    Array.isArray(item.url)
+                                                        ? item.url[0]
+                                                        : item.url
+                                                }
+                                            >
+                                                <Globe2 size={15} />{' '}
+                                                <p className="text-sm">
+                                                    Website
+                                                </p>
+                                            </CustomLink>
+                                        ) : null}
+                                    </div>
                                 </div>
-                            </div>
-                        )
-                    })}
+                            )
+                        })}
+                    </div>
                 </div>
-            </div>
+            </motion.div>
         </div>
     )
 }
