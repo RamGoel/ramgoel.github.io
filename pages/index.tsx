c0ac754f6fbe81b52e9e160d5d6d4266e39adc08
Ram Goel <rgoel766@gmail.com>
2025-04-06 11:33:56 +0530

    feat: add location

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,7 +25,7 @@ export default function Home() {
                 <div className="flex flex-col gap-1">
                     <h1 className="text-xl font-medium">Ram Goel</h1>
                     <p className="text-neutral-400 text-sm mb-1">
-                        Software Engineer
+                        Bangalore, India
                     </p>
                     <Link
                         target="_blank"
