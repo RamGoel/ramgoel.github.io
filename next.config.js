559dc665587f663608a1cd30abfd1b8eaf2db187
Ram Goel <rgoel766@gmail.com>
2025-04-26 18:11:15 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -16,6 +16,11 @@ const nextConfig = {
                 destination: 'https://crustdata-neon.vercel.app/',
                 permanent: true,
             },
+             {
+        source: '/hackathon', // the page you want to redirect from
+        destination: 'https://hackathon-tau-red.vercel.app/', // where you want to redirect to
+        permanent: true, // true = 308 redirect (good for SEO if it's permanent)
+          }
         ]
     },
 }
