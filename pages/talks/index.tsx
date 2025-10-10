60e63a4aeb6ee263f94001e39581b2f176495ff6
ram-sarvam <rgoel766@gmail.com>
2025-10-10 13:34:01 +0530

    fix: script

--- pages/talks/index.tsx
+++ pages/talks/index.tsx
@@ -2,11 +2,11 @@ import Script from 'next/script'
 
 export default function Talks() {
     return (
-       <div className='h-screen w-screen'>
-<div className='w-full h-full absolute bottom-0 right-0'>
-<atoms-widget assistant-id="68e7fbe9902dcd1149e4ced8"></atoms-widget><script src="https://unpkg.com/atoms-widget-core@latest/dist/embed/widget.umd.js"></script>
-
-</div>
-       </div>
+        <div className="h-screen w-screen">
+            <div className="w-full h-full absolute bottom-0 right-0">
+                <atoms-widget assistant-id="68e7fbe9902dcd1149e4ced8"></atoms-widget>
+                <Script src="https://unpkg.com/atoms-widget-core@latest/dist/embed/widget.umd.js"></Script>
+            </div>
+        </div>
     )
 }
