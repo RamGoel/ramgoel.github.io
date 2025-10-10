b668ac64408ed2a55dcbd73d6c5f8b41b3f425a0
ram-sarvam <rgoel766@gmail.com>
2025-10-10 13:36:35 +0530

    fix: element issue

--- pages/talks/index.tsx
+++ pages/talks/index.tsx
@@ -1,11 +1,22 @@
 import Script from 'next/script'
 
+// Declare the custom element for TypeScript
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'atoms-widget': {
+        'assistant-id': string;
+      };
+    }
+  }
+}
+
 export default function Talks() {
     return (
         <div className="h-screen w-screen">
+            <Script src="https://unpkg.com/atoms-widget-core@latest/dist/embed/widget.umd.js" strategy="beforeInteractive" />
             <div className="w-full h-full absolute bottom-0 right-0">
                 <atoms-widget assistant-id="68e7fbe9902dcd1149e4ced8"></atoms-widget>
-                <Script src="https://unpkg.com/atoms-widget-core@latest/dist/embed/widget.umd.js"></Script>
             </div>
         </div>
     )
