bb758e81c35db1a1ece864aefbbb352c92c588a1
ram-sarvam <rgoel766@gmail.com>
2026-01-15 00:06:58 +0530

    fix: remove dead code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import AnimatedWrapper from '@/components/AnimatedWrapper'
 import CustomTooltip from '@/components/custom-tooltip'
-import { ThemeProvider } from '@/contexts/ThemeContext'
 import '@/styles/globals.css'
 import { Analytics } from '@vercel/analytics/react'
 import type { AppProps } from 'next/app'
@@ -26,26 +25,24 @@ export default function App({ Component, pageProps }: AppProps) {
     ]
     const isPageRoute = pageRoutes.includes(pathname)
     return (
-        <ThemeProvider>
-            <main className={`${font.className} font-normal`}>
-                <title>Ram Goel</title>
-                {isPageRoute ? (
-                    <Component {...pageProps} />
-                ) : (
-                    <>
-                        <div className="flex flex-col lg:flex-row items-stretch bg-zinc-950 text-zinc-100 min-h-screen">
-                            <div className="w-full h-[1px] lg:h-full lg:w-[1px] bg-gradient-to-r lg:bg-gradient-to-b from-zinc-800 to-transparent" />
-                            <div className="h-[100vh] overflow-auto w-full">
-                                <div className="p-5 lg:p-10 lg:w-[50%] mx-auto w-full">
-                                    <Component {...pageProps} />
-                                </div>
+        <main className={`${font.className} font-normal`}>
+            <title>Ram Goel</title>
+            {isPageRoute ? (
+                <Component {...pageProps} />
+            ) : (
+                <>
+                    <div className="flex flex-col lg:flex-row items-stretch bg-zinc-950 text-zinc-100 min-h-screen">
+                        <div className="w-full h-[1px] lg:h-full lg:w-[1px] bg-gradient-to-r lg:bg-gradient-to-b from-zinc-800 to-transparent" />
+                        <div className="h-[100vh] overflow-auto w-full">
+                            <div className="p-5 lg:p-10 lg:w-[50%] mx-auto w-full">
+                                <Component {...pageProps} />
                             </div>
                         </div>
-                        <CustomTooltip id="hover-tooltip" />
-                        <Analytics />
-                    </>
-                )}
-            </main>
-        </ThemeProvider>
+                    </div>
+                    <CustomTooltip id="hover-tooltip" />
+                    <Analytics />
+                </>
+            )}
+        </main>
     )
 }
